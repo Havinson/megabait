dd16e2d0733e72f8998c7948ed173fc3904c2b47
Igor Khavinson <havinson1988@gmail.com>
2019-02-07 19:39:33 +0200

    first commit

---
+++ src/main/java/com/megabait/megabait/MegabaitApplication.java
@@ -0,0 +1,14 @@
+package com.megabait.megabait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegabaitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegabaitApplication.class, args);
+	}
+
+}
+
